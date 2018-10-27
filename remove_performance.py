5ba21bac69d994ab26fd5f13c1f862bef264943f
Anshul Gupta <anshul624.gupta@gmail.com>
2018-10-27 18:46:12 +0530

    Update remove_performance.py

--- remove_performance.py
+++ remove_performance.py
@@ -1,11 +1,11 @@
 import pickle
 import os
 
-perf = open('performances2.txt','r').read()
+perf = open('performances.txt','r').read()
 perf = perf.split()
 
 vids = []
-for csv in os.listdir('../Anshul/cuts_csv'):
+for csv in os.listdir('../TED/cuts_csv'):
 	vids.append(csv)
 
 
@@ -14,7 +14,7 @@ for csv in vids:
 	for p in perf:
 		if p in csv:
 			print csv
-			os.rename('../Anshul/cuts_csv/' + csv, '../Anshul/removed_csv/' + csv)
+			os.rename('../TED/cuts_csv/' + csv, '../TED/removed_csv/' + csv)
 			c+=1
 
 print ''
