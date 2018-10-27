4ee1f4a8628a59b4b66d7924b9d8d272b4721412
Anshul Gupta <anshul624.gupta@gmail.com>
2018-10-27 17:56:07 +0530

    Update get_tags.py

--- get_tags.py
+++ get_tags.py
@@ -23,6 +23,7 @@ print 'total len:', len(df.values)
 print 'total len:', len(df_tr.values)
 
 
+# checks if there are any repeats in the data
 
 unique, counts = np.unique(url2, return_counts=True)
 d = dict(zip(unique, counts))
@@ -31,6 +32,7 @@ for i in d:
 		print i
 
 
+# Get tags for every talk
 
 tags = df['tags']
 
@@ -56,73 +58,12 @@ set_tags = set(all_tags)
 freq = [all_tags.count(w) for w in set_tags]
 tf = {a:i for a,i in zip(set_tags,freq)}
 
-#print tf['culture']
+
+# Prints all tags by frequency of occurence
 
 tf = sorted(tf.iteritems(), key=lambda (k,v): (v,k))
-
 print tf
 
 
-# change number of classes here
-
-'''
-tf2=[]
-tags_50=[]
-for a,i in tf[-50:]:
-	tf2.append((a,i))
-	tags_50.append(a)
-
-with open('tags_50', 'wb') as fp:
-	pickle.dump(tags_50, fp)
-#print tf2
-#print len(tf2)
-
-# get number of talks with top 50 tags
-
-
-def intersection(lst1, lst2):
- 
-    # Use of hybrid method
-    temp = set(lst2)
-    lst3 = [value for value in lst1 if value in temp]
-    return lst3	
-
-c=0
-
-for t in talk_tags:
-	if(intersection(t, tags_50) != []):
-		c+=1
-
-print 'total talks:', len(talk_tags)
-print 'count of talks within 50 tags:', c
-
-
-# get new tag list with only those in top 50
-
-for i,tt in enumerate(talk_tags):
-	for j,t in enumerate(tt):
-		if t not in tags_50:
-			talk_tags[i][j] = 'UNK'
-
-#for tt in talk_tags:
-#	tt.append('<eos>')
-	
-print talk_tags[0]
-print len(talk_tags)
-
-'''
 with open('talk_alltags','wb') as fp:
 	pickle.dump(talk_tags,fp)
-
-
-def get_talk_with_tag(talk_tags, tag):
-	
-	talks = []
-
-	for i,tt in enumerate(talk_tags):
-		if tag in tt:
-			talks.append(i)
-
-	
-	return talks
-	
